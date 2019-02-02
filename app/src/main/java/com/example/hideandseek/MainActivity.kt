8c652e615eaf73291323dc3e90e1f40dc4c2f119
Developerani <developer.aani@gmail.com>
2019-02-03 01:07:17 +0530

    UI Updated

--- app/src/main/java/com/example/hideandseek/MainActivity.kt
+++ app/src/main/java/com/example/hideandseek/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.hideandseek
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        submit_otp.setOnClickListener {
+            var phone_no:String = phone_no.text.toString()
+            var otp:String = otp.text.toString()
+            Toast.makeText(this,phone_no+" OTP:"+otp,Toast.LENGTH_SHORT).show()
+        }
     }
 }
